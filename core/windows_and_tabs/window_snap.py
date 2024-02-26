8bc96fa1a2acc9a40dbab85b88a75494bfedad07
glemvik <gunvor.lemvik@gmail.com>
2024-02-26 17:05:44 +0100

    Update to default shortcuts for rectangle

--- core/windows_and_tabs/window_snap.py
+++ core/windows_and_tabs/window_snap.py
@@ -254,36 +254,36 @@ class Actions:
     ########################################################
     def snap_rectangle_full() -> None:
         """Make the active window go full screen"""
-        actions.key("cmd-alt-f")
+        actions.key("ctrl-alt-enter")
 
     def snap_rectangle_top_half() -> None:
         """Snap the active window to top half"""
-        actions.key("cmd-alt-up")
+        actions.key("ctrl-alt-up")
 
     def snap_rectangle_bottom_half() -> None:
         """Snap the active window to bottom half"""
-        actions.key("cmd-alt-down")
+        actions.key("ctrl-alt-down")
 
     def snap_rectangle_left_half() -> None:
         """Snap the active window to left half"""
-        actions.key("cmd-alt-left")
+        actions.key("ctrl-alt-left")
 
     def snap_rectangle_right_half() -> None:
         """Snap the active window to right half"""
-        actions.key("cmd-alt-right")
+        actions.key("ctrl-alt-right")
 
     def snap_rectangle_left_top() -> None:
         """Snap the active window to left top"""
-        actions.key("cmd-ctrl-left")
+        actions.key("ctrl-alt-u")
 
     def snap_rectangle_left_bottom() -> None:
         """Snap the active window to left bottom"""
-        actions.key("cmd-ctrl-shift-left")
+        actions.key("ctrl-alt-j")
 
     def snap_rectangle_right_top() -> None:
         """Snap the active window to right top"""
-        actions.key("cmd-ctrl-right")
+        actions.key("ctrl-alt-i")
 
     def snap_rectangle_right_bottom() -> None:
         """Snap the active window to right bottom"""
-        actions.key("cmd-ctrl-shift-right")
+        actions.key("ctrl-alt-k")
